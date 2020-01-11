b9a2cf28cbf0246bcf477e4e487709149c60547e
Stephen Sanchez <stephensanchez05@gmail.com>
2020-01-11 02:28:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,8 +8,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     array = []
     get_page = Nokogiri::HTML(open("#{index_url}"))
-    get_page.css(".student-card")
-    index.each do |student|
+    get_page.css(".student-card").each do |student|
       student_name = student.css("h4").text
       student_location = student.css("p").text
       profile_url = "NY"
